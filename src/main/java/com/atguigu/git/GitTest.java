9f862d577df0a6f28feaa0ae11ed8445bbfa8e2a
yang <1527644251@qq.com>
2022-07-30 17:40:29 +0800

    hot-fix second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -11,6 +11,6 @@ public class GitTest {
         System.out.println("hello,world");
         System.out.println("hello,world");
         System.out.println("hello,world3");
-        System.out.println("hello,world3");
+        System.out.println("hot-fix test");
     }
 }
