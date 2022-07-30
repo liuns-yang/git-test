252aa8c5a26bb132e59ba18e8693d87a42e14f15
yang <1527644251@qq.com>
2022-07-30 12:32:08 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -7,5 +7,8 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,world");
+        System.out.println("hello,world");
+        System.out.println("hello,world");
+        System.out.println("hello,world");
     }
 }
