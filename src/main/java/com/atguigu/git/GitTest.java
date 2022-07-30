9e34d7be0de0c6d04f591e4948c98bd298ad9456
yang <1527644251@qq.com>
2022-07-30 16:49:16 +0800

    hot-fix first commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,5 +10,7 @@ public class GitTest {
         System.out.println("hello,world");
         System.out.println("hello,world");
         System.out.println("hello,world");
+        System.out.println("hello,world3");
+        System.out.println("hello,world3");
     }
 }
