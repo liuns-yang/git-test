4b7524a6f138c3fe8b4c360c4cd5e3be6828e1f7
yang <1527644251@qq.com>
2022-07-31 11:55:07 +0800

    gitee commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -16,6 +16,8 @@ public class GitTest {
         System.out.println("push test");
         System.out.println("pull test");
         System.out.println("pull test1");
-        System.out.println("pull test2");
+        System.out.println("mayun test2");
+        System.out.println("Gitee test2");
+        System.out.println("Gitee test2");
     }
 }
